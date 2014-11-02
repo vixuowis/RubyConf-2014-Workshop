d8379368c687bb6ff6fc4037a8fe87090832ef26
VIXUOWIS <vixuowis@hotmail.com>
2014-11-02 15:05:05 +0800

    plus and minus

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,5 +1,14 @@
 module Calc
   def self.eval(string)
-    string.to_i
+
+  	if string.include? "+"
+  		a,b = string.split("+")
+  		a.to_i + b.to_i
+  	elsif string.include? "-"
+  		a,b = string.split("-")
+  		a.to_i - b.to_i
+  	else
+  		string.to_i
+  	end
   end
 end
