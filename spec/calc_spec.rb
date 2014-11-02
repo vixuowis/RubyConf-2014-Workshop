e2044cf63526dc44aef38b0e791e889e2691ab5c
SouthWolf <wp.southwolf@gmail.com>
2014-11-02 14:44:15 +0800

    Both spec pass

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -4,6 +4,12 @@ require 'calc'
 describe 'calc' do
 
   it 'should eval 2 to 2' do
-    Calc.eval('2') == 2
+    # Calc.eval('2').should eq 2
+    expect(Calc.eval('2')).to eq 2
+  end
+
+  it 'should eval 11 to 11' do
+    # Calc.eval('11').sould eq 11
+    expect(Calc.eval('11')).to eq 11
   end
 end
