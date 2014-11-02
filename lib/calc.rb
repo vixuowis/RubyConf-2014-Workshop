5afe4de95936395b1c7663f483f4621d8c72a048
VIXUOWIS <vixuowis@hotmail.com>
2014-11-02 18:45:27 +0800

    Do string work

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,21 +1,21 @@
 module Calc
 
   def self.eval(string,doit=true)
-    puts "eval #{string}"
+    # puts "eval #{string}"
 
     if string =~ /\(.*\)/ and doit
-      string = parse_blanket(string)
+      string = parse_brackets(string)
     end
 
     if string =~ /(.*)(\+|\-)(.*)/
-      puts "-- #{$1},#{$2},#{$3}"
+      # puts "-- #{$1},#{$2},#{$3}"
       a,op,b = eval($1,doit), $2, eval($3,doit)
       case op
         when "+" then a + b
         when "-" then (a - b) < 0 ? 0 : a - b
       end
     elsif string =~ /(.*)(\*|\/)(.*)/
-      puts "-- #{$1},#{$2},#{$3}"
+      # puts "-- #{$1},#{$2},#{$3}"
       a,op,b = eval($1,doit), $2, eval($3,doit)
       case op
         when "*" then a * b
@@ -26,19 +26,19 @@ module Calc
   	end
   end
 
-  def self.parse_blanket(string)
+  def self.parse_brackets(string)
     if string =~ /(.*)\((.*)\)(.*)/
-      puts "pb ($2) #{$1},#{$2},#{$3}"
-      deblanket_rs = $1 + parse_blanket($2).to_s + $3
-      puts "pb ($2-rs) #{deblanket_rs}"
-      return deblanket_rs
+      # puts "pb ($2) #{$1},#{$2},#{$3}"
+      result = $1 + parse_brackets($2).to_s + $3
+      # puts "pb ($2-rs) #{result}"
+      return result
     else
-      puts "pb (string) #{string}"
+      # puts "pb (string) #{string}"
       result = eval(string,false).to_s
-      puts "pb (string-rs) #{result}"
+      # puts "pb (string-rs) #{result}"
       return result
     end
   end
 end
 
-puts Calc.eval("(8-4)+(6-8)/3")
+# puts Calc.eval("(8-4)+(6-8)/3")
