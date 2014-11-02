a50b2090b51fb88a3bfd200195a9e7700a74d68e
SouthWolf <wp.southwolf@gmail.com>
2014-11-02 15:33:04 +0800

    refactor spec and add minus result <0

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -3,41 +3,46 @@ require 'calc'
 
 describe 'calc' do
 
-  # Basic digits
-  it 'should eval 2 to 2' do
-    # Calc.eval('2').should eq 2 Deprecation!
-    expect(Calc.eval('2')).to eq 2
+  context 'Basic digits' do
+    it 'should eval 2 to 2' do
+      # Calc.eval('2').should eq 2 Deprecation!
+      expect(Calc.eval('2')).to eq 2
+    end
+    it 'should eval 11 to 11' do
+      # Calc.eval('11').sould eq 11
+      expect(Calc.eval('11')).to eq 11
+    end
   end
 
-  it 'should eval 11 to 11' do
-    # Calc.eval('11').sould eq 11
-    expect(Calc.eval('11')).to eq 11
-  end
+  context 'Basic calculations' do
+    it 'should eval 1+2 to 3' do
+      expect(Calc.eval('1+2')).to eq 3
+    end
 
+    it 'should eval 3-1 to 2' do
+      expect(Calc.eval('3-1')).to eq 2
+    end
 
-  # Basic calculations
-  it 'should eval 1+2 to 3' do
-    expect(Calc.eval('1+2')).to eq 3
-  end
+    it 'should eval 1-1 to 0' do
+      expect(Calc.eval('1-1')).to eq 0
+    end 
 
-  it 'should eval 3-1 to 2' do
-    expect(Calc.eval('3-1')).to eq 2
-  end
+    it 'should eval 2*1 to 2' do
+      expect(Calc.eval('2*1')).to eq 2
+    end
 
-  it 'should eval 1-1 to 0' do
-    expect(Calc.eval('1-1')).to eq 0
-  end 
-
-  it 'should eval 2*1 to 2' do
-    expect(Calc.eval('2*1')).to eq 2
-  end
-
-  it 'should eval 4/2 to 2' do
-    expect(Calc.eval('4/2')).to eq 2
-  end
-
+    it 'should eval 4/2 to 2' do
+      expect(Calc.eval('4/2')).to eq 2
+    end
+  
     # integer division
-  it 'should eval 4/3 to 1' do
-    expect(Calc.eval('4/3')).to eq 1
+    it 'should eval 4/3 to 1' do
+      expect(Calc.eval('4/3')).to eq 1
+    end
   end
+
+  context 'Minus result < 0' do
+    it 'should eval 3-5 to 0' do
+      expect(Calc.eval('3-5')).to eq 0
+    end
 end
