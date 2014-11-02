da51702c5df2fd47e29e8424d5428fba2f32664f
SouthWolf <wp.southwolf@gmail.com>
2014-11-02 14:48:03 +0800

    plus calc spec

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -3,13 +3,20 @@ require 'calc'
 
 describe 'calc' do
 
+  # Basic digits
   it 'should eval 2 to 2' do
-    # Calc.eval('2').should eq 2
+    # Calc.eval('2').should eq 2 Deprecation!
     expect(Calc.eval('2')).to eq 2
   end
 
   it 'should eval 11 to 11' do
     # Calc.eval('11').sould eq 11
     expect(Calc.eval('11')).to eq 11
+  end
+
+
+  # Basic calculation
+  it 'should eval 1+2 to 3' do
+    expect(Calc.eval('1+2')).to eq 3
   end
 end
